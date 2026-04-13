bc168026487b44559e0129fe8f9468dddaf5d9aa
Prince Sharma <princesharma4867@gmail.com>
2026-04-13 11:05:37 +0530

    initial commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -12,34 +12,41 @@ export async function middleware(req: NextRequest){
     const response = NextResponse.next();
     const matchedRoute = routeAccess.find(route => pathname.startsWith(route.path));
     response.cookies.set("x-log",pathname || "no data",{path: "/"});
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/user/profile`,{
-        headers: {
-            cookie: req.headers.get("cookie") || "",
+
+    let res;
+    try{
+        res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/user/profile`,{
+            headers: {
+                cookie: req.headers.get("cookie") || "",
+            }
+        });
+    } catch(error) {
+        response.cookies.set("x-error", "true");
+        response.cookies.set("x-message", "something went wrong");
+    } finally {
+        if(!res?.ok){
+            response.cookies.set("x-auth","false");
+            // return response;
+        } else {
+            response.cookies.set("x-auth","true"); 
         }
-    });
 
-    if(!res?.ok){
-        response.cookies.set("x-auth","false");
-        // return response;
-    } else {
-        response.cookies.set("x-auth","true"); 
-    }
+        if(!matchedRoute){
+            response.cookies.set("x-role","authorized");
+            return response;
+        }
 
-    if(!matchedRoute){
-        response.cookies.set("x-role","authorized");
+        const { data } = await res?.json();
+        
+        
+        if(!matchedRoute.roles.includes(data?.role)){
+            response.cookies.set("x-role","unauthorized");
+        } else {
+            response.cookies.set("x-role","authorized");
+        }
+    
         return response;
     }
-
-    const { data } = await res.json();
-    
-    
-    if(!matchedRoute.roles.includes(data?.role)){
-        response.cookies.set("x-role","unauthorized");
-    } else {
-        response.cookies.set("x-role","authorized");
-    }
-    
-    return response;
 }
 
 export const config = {
