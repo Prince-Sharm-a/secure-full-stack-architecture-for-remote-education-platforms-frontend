effa2cd3cf0c9d9d50323e68084e865e1c74dcd0
Prince Sharma <princesharma4867@gmail.com>
2026-04-13 11:18:54 +0530

    initial commit

--- src/middleware.ts
+++ src/middleware.ts
@@ -13,33 +13,32 @@ export async function middleware(req: NextRequest){
     const matchedRoute = routeAccess.find(route => pathname.startsWith(route.path));
     response.cookies.set("x-log",pathname || "no data",{path: "/"});
 
-    let res;
+    let data;
     try{
-        res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/user/profile`,{
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/api/v1/user/profile`,{
             headers: {
                 cookie: req.headers.get("cookie") || "",
             }
         });
-    } catch(error) {
-        response.cookies.set("x-error", "true");
-        response.cookies.set("x-message", "something went wrong");
-    } finally {
         if(!res?.ok){
             response.cookies.set("x-auth","false");
             // return response;
         } else {
             response.cookies.set("x-auth","true"); 
         }
+        data = await res.json();
+        
+    } catch(error) {
+        response.cookies.set("x-error", "true");
+        response.cookies.set("x-message", "something went wrong");
+    } finally {
 
         if(!matchedRoute){
             response.cookies.set("x-role","authorized");
             return response;
         }
-
-        const { data } = await res?.json();
         
-        
-        if(!matchedRoute.roles.includes(data?.role)){
+        if(!matchedRoute.roles.includes(data?.data?.role)){
             response.cookies.set("x-role","unauthorized");
         } else {
             response.cookies.set("x-role","authorized");
