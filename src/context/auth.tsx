24ee7a7e32d6e1fd18a7d62b8142578c01d01e85
Prince Sharma <princesharma4867@gmail.com>
2026-04-27 22:50:30 +0530

    initial commit

--- src/context/auth.tsx
+++ src/context/auth.tsx
@@ -15,7 +15,6 @@ export const AuthProvider = ({ children } : ChildrenType) => {
         }
 
         window.addEventListener("storage", handleStorage);
-        return () => window.removeEventListener("storage", handleStorage);
     },[])
     
     return (
